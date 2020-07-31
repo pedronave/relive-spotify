9b54bbf1755c2f41329a968b1239e81c08bd7ba8
Pedro <pedronavelopes@gmail.com>
2020-07-31 16:24:12 +0100

    Updated production uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  redirectUri: 'http://relive-spotify.netlify.app/callback',
+  redirectUri: 'https://relive-spotify.navelopes.com/callback',
   clientId: 'ef1a41b86dbe45d0ada5604fe69f30b2',
 };
